85cd11b75f7de0f264690326d98aa91e1756f8f3
freelearncoding <86627409+freelearncoding@users.noreply.github.com>
2022-05-23 02:07:31 +0530

    new ui - barcode scanner
    Created material barcode tab. revised packages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,78 @@
-import { Component, VERSION, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
+import { NgxScannerQrcodeComponent } from 'ngx-scanner-qrcode';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
   links = ['Bar Code', 'QR Code', 'Read Me'];
   activeLink = this.links[0];
   background: ThemePalette = undefined;
   isError = false;
-  publ output: string;
+  output: string;
+
+
+  @ViewChild(BarcodeScannerLivestreamComponent) barcodeScanner: BarcodeScannerLivestreamComponent;
+  @ViewChild(NgxScannerQrcodeComponent) qrcodeh: NgxScannerQrcodeComponent;
+  // @ViewChild('qrcodeh') QRScanner: NgxScannerQrcodeComponent;
+
+  barcodeValue: any;
+  constructor(private cdref: ChangeDetectorRef) { }
+
+
+  ngAfterContentChecked() {
+    this.cdref.detectChanges();
+  }
+
+  ngAfterViewInit() {
+    this.barcodeScanner.start();
+  }
+
 
   toggleBackground() {
     this.background = this.background ? undefined : 'primary';
   }
 
-  addLink() {
-    this.links.push(`Link ${this.links.length + 1}`);
+  clickTab(link: any) {
+
+    this.activeLink = link.target.textContent.trim().toString().replace('qr_code', '');
+   
+    switch (this.activeLink) {
+      case 'Bar Code':
+        if (this.qrcodeh) {
+          this.qrcodeh.stop();
+        }
+        setTimeout(() => {
+          if (this.barcodeScanner) {
+            this.barcodeScanner.start();
+          }
+        }, 1000);
+        break;
+      case 'QR Code':
+        if (this.barcodeScanner) {
+          this.barcodeScanner.stop();
+        }
+        setTimeout(() => {
+          if (this.qrcodeh) {
+            this.qrcodeh.start();
+          }
+        }, 1000);
+        break;
+      default:
+        break;
+    }
   }
-  
-  @ViewChild(BarcodeScannerLivestreamComponent) barcodeScanner: BarcodeScannerLivestreamComponent;
-  barcodeValue: any;
 
   onError(error: any) {
     console.error(error);
     this.isError = true;
-  }
-
-  ngAfterViewInit() {
-    this.barcodeScanner.start();
   }
 
   onValueChanges(result: any, isQR: boolean) {
@@ -45,14 +85,30 @@ export class AppComponent implements AfterViewInit {
     console.log(result);
   }
 
-  onStarted(started: any) {
+  onStarted(started: any, isQR: boolean) {
     console.log(started);
+    if (isQR && this.qrcodeh && !this.qrcodeh.isStart && started) {
+      this.cdref.detectChanges();
+    } 
+    if (!isQR && this.barcodeScanner && !this.barcodeScanner.isStarted && started) {
+      this.cdref.detectChanges();
+    }
   }
 
-  
-  // barcodeValue: any;
-  // value!: string;
-  // @ViewChild(NgxBarcodeScannerComponent)
-  // barecodeScanner!: NgxBarcodeScannerComponent;
+  cameraToggle(isQR: boolean) {
+    if (isQR) {
+      this.qrcodeh.toggleCamera(); 
+    } else {
+      this.barcodeScanner.isStarted ? this.barcodeScanner.stop() : this.barcodeScanner.start();
+    }
+  }
+
+  getdisabled(isQR: boolean) {
+    if (isQR) {
+      return !(this.qrcodeh && this.qrcodeh.isStart); 
+    } else {
+      return !(this.barcodeScanner && this.barcodeScanner.isStarted) ;
+    }
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,37 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule, } from '@angular/platform-browser';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTabsModule } from '@angular/material/tabs';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { BarcodeScannerLivestreamModule } from 'ngx-barcode-scanner';
 import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 
+const materialModuleList: any[] = [
+  MatButtonModule, MatInputModule, MatTabsModule, MatToolbarModule, MatIconModule, MatCardModule
+]
+
+
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTabsModule,
+    materialModuleList,
     BarcodeScannerLivestreamModule,
     NgxScannerQrcodeModule
   ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
